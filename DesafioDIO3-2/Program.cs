4634dae15eeabbd4b14738af673d880265aa453c
Jonathan Santos <jonathan2010.10@hotmail.com>
2021-12-15 16:49:20 -0300

    Add project files.

---
+++ DesafioDIO3-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DesafioDIO3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int V = int.Parse(Console.ReadLine());
+            while (V > 50)
+            {
+                V = int.Parse(Console.ReadLine());
+            }
+            int[] N = new int[10];
+            if (V <= 50)
+            {
+                N[0] = V;
+            }
+
+            int cont = 1;
+
+            for (int i = 0; i < N.Length - 1; i++)
+            {
+                N[i + cont] += N[i] * 2;
+            }
+            for (int j = 0; j < N.Length; j++)
+            {
+                Console.WriteLine($"N[{j}] = " + N[j]);
+            }
+
+        }
+    }
+}
